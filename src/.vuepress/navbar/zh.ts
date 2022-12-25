d3cf229b241c89b3156ad70eac3fd3c69dced60f
guohaoyang <haoyang.guo@duobei.com>
2022-12-25 23:57:37 +0800

    feat: *

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -2,32 +2,11 @@ import {navbar} from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
-  {text: "Java", icon: "discover", link: "/java/"},
-  {text: "Golang", icon: "discover", link: "/golang/"},
-  {text: "数据库", icon: "discover", link: "/db/"},
-  {text: "Linux", icon: "discover", link: "/linux/"},
-  {text: "数据结构算法", icon: "discover", link: "/algo/"},
-  {text: "基础知识", icon: "discover", link: "/knowledge/"},
-  {text: "其它", icon: "discover", link: "/other/"},
-  {
-    text: "数据库",
-    icon: "discover",
-    prefix: "/goods/",
-    children: [
-      {
-        text: "苹果",
-        icon: "edit",
-        prefix: "demo1/",
-        children: [
-          {text: "苹果1", icon: "edit", link: "1"},
-          {text: "苹果2", icon: "edit", link: "2"},
-        ]
-      }
-    ],
-  }
-  // {
-  //   text: "V2 文档",
-  //   icon: "note",
-  //   link: "https://vuepress-theme-hope.github.io/v2/zh/",
-  // },
+  {text: "Java",  link: "/java/"},
+  {text: "Golang",  link: "/golang/"},
+  {text: "数据库",  link: "/db/"},
+  {text: "Linux",  link: "/linux/"},
+  {text: "数据结构算法",  link: "/algo/"},
+  {text: "基础知识",  link: "/knowledge/"},
+  {text: "其它",  link: "/other/"},
 ]);
